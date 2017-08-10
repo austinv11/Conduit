50110ae0394f5384d91fb74fc73eb0bd92b21782
austinv11 <austinv11@gmail.com>
2017-08-09 22:47:31 -0400

    Add test file

---
+++ src/test/java/conduit/test/Test.java
@@ -0,0 +1,17 @@
+package conduit.test;
+
+import conduit.Conduit;
+import conduit.common.Header;
+
+public class Test {
+	
+	@org.junit.Test
+	public void test() {
+		Conduit conduit = new Conduit.Builder("https://google.com/").build();
+		conduit.get("some/request", hrb -> hrb.addHeader(Header.from("user-agent", "idek"))
+			.setPacketData(new byte[]{1, 3, 3, 7})).subscribe(r -> {
+			byte[] data = r.getResponseData();
+		});
+		String.jo
+	}
+}
